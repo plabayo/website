374b99c38eb3e7460c75fc0589ff4ef2488800d5
glendc <glen.decauwsemaecker@otainsight.com>
2022-04-09 21:38:21 +0200

    add favicons src & result to website

---
+++ design/copy_favicon_to_assets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -f favicon.ico ../static/favicon.ico
+cp -f favicon_opt.svg ../static/icon.svg
+cp -f apple-touch-icon.png ../static/apple-touch-icon.png
+cp -f icon-192.png ../static/icon-192.png
+cp -f icon-512.png ../static/icon-512.png
