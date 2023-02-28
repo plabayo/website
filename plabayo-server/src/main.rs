466dfcf097d2cd1d9863365d9c24df89f5526326
glendc <glen.decauwsemaecker@otainsight.com>
2023-02-28 02:55:52 +0100

    add first dirty plabayo-website core structure

--- plabayo-server/src/main.rs
+++ plabayo-server/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use clap::Parser;
+
+use std::{
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
+
+/// Plabayo stand-alone web server, serving the official Plabayo website.
+/// As an alternative to the (cloudflare) worker version found in the /plabayo-worker directory.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// interface to bind to
+    #[arg(short, long, default_value = "127.0.0.1")]
+    interface: String,
+
+    /// Port to listen on
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/style.css", get(stylesheet));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let interface = IpAddr::from_str(&args.interface).unwrap();
+    let addr = SocketAddr::from((interface, args.port));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> Response {
+    let mut resp = plabayo_website::index().into_response();
+    resp.headers_mut()
+        .insert("Content-Type", "text/html".parse().unwrap());
+    resp
+}
+
+async fn stylesheet() -> Response {
+    let mut resp = plabayo_website::stylesheet().into_response();
+    resp.headers_mut()
+        .insert("Content-Type", "text/css".parse().unwrap());
+    resp
 }
--- plabayo-website/src/lib.rs
+++ plabayo-website/src/lib.rs
@@ -1,14 +1,87 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use maud::html;
+
+/*
+<html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>ushift</title>
+        </head>
+        <body>
+            <main>
+                <section>
+                <h1><a href="https://github.com/plabayo/ushift">ushift</a></h1>
+                <article>
+                <h2>Libraries and framework to liberate the data of this world.</h2>
+                <p>
+                    Documentation: <a href="https://docs.rs/ushift">https://docs.rs/ushift</a>.
+                <p>
+                    A set of libraries to aid you in the data extraction and transformation of
+                    liberated data around the world. Libraries can be used individually or
+                    as a whole to write a spider from start to finish.
+                <p>
+                    Follow the progress and learn how to get involved
+                    by going to the ushift code repository at:
+                    <a href="https://github.com/plabayo/ushift">https://github.com/plabayo/ushift</a>.
+                </p>
+                <p>
+                    A Free and Open Source project actively researched and developed by
+                    <a href="https://www.plabayo.tech/">Plabayo.tech</a>.
+                </p>
+                </article>
+                </section>
+            </main>
+            </body>
+        </html>
+ */
+
+pub fn index() -> String {
+    let html = html! {
+        html lang="en" {
+            head { 
+                meta charset="UTF-8";
+                meta name="viewport" content="width=device-width, initial-scale=1.0";
+                title { "ushift" }
+                link rel="stylesheet" href="/style.css" {}
+            }
+            body {
+                main {
+                    section {
+                        h1 {
+                            a href="https://plabayo.tech/" { "Plabayo" }
+                        }
+                        article {
+                            p {
+                                "Plabayo was co-founded in 2021 by "
+                                a href="https://www.elizadc.me/" { "Elizabeth C. Gonzales Belsuzarri" }
+                                " and "
+                                a href="https://www.glendc.com/" { "Glen Henri J. De Cauwsemaecker" }
+                                " as a Free and Open Source Software (FOSS) development and consultancy studio."
+                            }
+                            p {
+                                "We are located in Ghent, "
+                                "Belgium and are available with events on premise within Belgium. "
+                                "Remotely we are open to any location and timezone."
+                            }
+                            p {
+                                "Education, Privacy and transparency is at the core of our mission."
+                                "We take our craft serious and work on the fringes of playfulness."
+                            }
+                            p {
+                                "If you like or work and would like to donate to us, you can do so at "
+                                a href="https://github.com/sponsors/plabayo" { "https://github.com/sponsors/plabayo" }
+                                "."
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    html.into_string()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn stylesheet() -> &'static str {
+    include_str!("../static/style.css")
 }
